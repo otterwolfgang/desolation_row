a490cdcc0c4c3f441c1d99f518c383723b4b0270
Wolfgang Otter <otter.wolfgang@gmail.com>
2018-12-03 22:23:08 +0100

    Started working on automatically updating the legend for the word trends plot in tab_ngram

--- scripts/tab_ngrams.py
+++ scripts/tab_ngrams.py
@@ -13,7 +13,7 @@ from nltk import pos_tag
 from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
 
 from bokeh.layouts import layout, widgetbox
-from bokeh.models import ColumnDataSource, LinearColorMapper
+from bokeh.models import ColumnDataSource, LinearColorMapper, Legend, LegendItem
 from bokeh.models.widgets import Panel, Tabs, TextInput
 from bokeh.palettes import magma, RdPu9
 from bokeh.plotting import figure
@@ -315,7 +315,8 @@ def tab_ngrams(df, plot_width, plot_height):
         # Create the step glyph for the first word
         first = plot.step(
             x='Year', y='Word', source=src01, mode='after',
-            color='salmon', alpha=0.7, line_width=2
+            color='salmon', alpha=0.7, line_width=2,
+            # legend=w01_input.value
         )
 
         # Create the step glyph for the second word
@@ -341,15 +342,7 @@ def tab_ngrams(df, plot_width, plot_height):
     # print('love', word_list['love'][:5])
 
 
-    # Function to update the word trends to specific words
-    def update_trends(attr, old, new):
-        src01_new = freq_over_years(df, w01_input.value, all_years(df))
-        src02_new = freq_over_years(df, w02_input.value, all_years(df))
-        src03_new = freq_over_years(df, w03_input.value, all_years(df))
 
-        src01_trends.data.update(src01_new.data)
-        src02_trends.data.update(src02_new.data)
-        src03_trends.data.update(src03_new.data)
 
 
     # Create plot 1 for word trends
@@ -358,13 +351,7 @@ def tab_ngrams(df, plot_width, plot_height):
     w02 = 'lord'
     w03 = 'jesus'
 
-    # Create three text input widget for displaying three words individually
-    w01_input = TextInput(title='Word No. 1:', value=w01, placeholder='type here')
-    w01_input.on_change('value', update_trends)
-    w02_input = TextInput(title='Word No. 2:', value=w02, placeholder='type here')
-    w02_input.on_change('value', update_trends)
-    w03_input = TextInput(title='Word No. 3:', value=w03, placeholder='type here')
-    w03_input.on_change('value', update_trends)
+
 
     src01_trends = freq_over_years(df, w01, all_years(df))
     src02_trends = freq_over_years(df, w02, all_years(df))
@@ -376,6 +363,38 @@ def tab_ngrams(df, plot_width, plot_height):
     )
 
     word_trends = style(word_trends)
+
+    def add_legend(plot, label):
+        li1 = LegendItem(label=label, renderers=[plot.renderers[5]])
+        # li2 = LegendItem(label='blue', renderers=[p1.renderers[1]])
+        # li3 = LegendItem(label='purple', renderers=[p1.renderers[2]])
+        legend1 = Legend(items=[li1], location='top_right')
+        plot.add_layout(legend1)
+
+    add_legend(word_trends, 'god')
+
+    # Function to update the word trends to specific words
+    def update_trends(attr, old, new):
+        src01_new = freq_over_years(df, w01_input.value, all_years(df))
+        src02_new = freq_over_years(df, w02_input.value, all_years(df))
+        src03_new = freq_over_years(df, w03_input.value, all_years(df))
+
+        src01_trends.data.update(src01_new.data)
+        src02_trends.data.update(src02_new.data)
+        src03_trends.data.update(src03_new.data)
+
+        add_legend(word_trends, w01_input.value)
+        add_legend(word_trends, w02_input.value)
+        add_legend(word_trends, w03_input.value)
+
+
+    # Create three text input widget for displaying three words individually
+    w01_input = TextInput(title='Word No. 1:', value=w01, placeholder='type here')
+    w01_input.on_change('value', update_trends)
+    w02_input = TextInput(title='Word No. 2:', value=w02, placeholder='type here')
+    w02_input.on_change('value', update_trends)
+    w03_input = TextInput(title='Word No. 3:', value=w03, placeholder='type here')
+    w03_input.on_change('value', update_trends)
 
     # Create plot 2 for the most popular words and their closest links
     num_links = 10
